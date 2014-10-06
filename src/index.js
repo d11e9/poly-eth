6221a74759a9b4bd08194f9def380e621aafef91
d11e9 <d11e9@turkd.net>
2014-10-06 21:01:35 +0000

    Fix proxying for go-ethereum eth object (ethereum.js)

--- src/index.js
+++ src/index.js
@@ -1,36 +1,14 @@
+
+var _ = require( 'underscore' )
+
 var polyeth = function(eth) {
 
   var checkClient = function(eth) {
     var UA = window.navigator.userAgent;
     if (UA.match( 'Aleth' )) return 'aleth';
-    if (UA.match( 'Ethereal')) return 'ethereal';    
+    if (UA.match( 'Ethereal')) return 'ethereal';
+    if (UA.match( 'Ethos')) return 'ethereal'; 
     return false;
-  }
-
-  // Custom jQuery.extend / _.defaults. Ideally to prevent requiring them as dependencies (for now).
-  var extend = function (target, source) {
-      var a = Object.create(target);
-      Object.keys(source).map(function (prop) {
-          prop in a && (a[prop] = source[prop]);
-      });
-      return a;
-  };
-
-
-  if (typeof window.env.note !== 'undefined' && typeof window.env.warn !== 'undefined') {
-
-    var _concat = function(){
-      var args = Array.prototype.slice.call(arguments, 0);
-      return args.map( function(arg){
-        return arg.toString();
-      }).join(' ') );
-    }
-    console.log = function(){
-      env.note( _concat(arguments) );
-    };
-    window.onerror = function() {
-      env.warn( _concat(arguments) );
-    }
   }
 
   // new poly-eth default APIs. will be overwitten if the method or 
@@ -54,13 +32,14 @@ var polyeth = function(eth) {
 
     ethereal: checkClient( eth ) == 'ethereal' && {
       eth: eth,
-      client: 'ethereal'
+      client: 'ethereal',
+      getKey: function(cb){ eth.key.then( cb ) }
     }
   }
 
   // TODO: This will no longer throw errors when mocketh etc are available.
   if (!eth) throw new Error( 'No Ethereum client.' );
-  else return extend( polyEth, ethObjectclients[checkClient(eth)] );
+  else return _.extend( clients[ checkClient( eth ) ], _.extend( polyEth, clients[ checkClient( eth ) ].eth ) );
 };
 
 
